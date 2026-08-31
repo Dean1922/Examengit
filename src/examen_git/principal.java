0f426a6f0f54e3cb75ee346767807cf45177c04b
patrick <patrick>
2026-08-31 06:38:28 -0400

    Creacion de los archivos utilidades y principal

---
+++ src/examen_git/principal.java
@@ -0,0 +1,7 @@
+package examen_git;
+public class principal {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/examen_git/utilidades.java
@@ -0,0 +1,4 @@
+package examen_git;
+public class utilidades {
+    
+}
