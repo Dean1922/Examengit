02a6bdd735eb851f245040ee9829288d9ebf62aa
patrick <patrick>
2026-08-31 06:41:25 -0400

    Creacion de los arreglos y menu en la clase principal

--- src/examen_git/principal.java
+++ src/examen_git/principal.java
@@ -1,7 +1,15 @@
 package examen_git;
 public class principal {
     public static void main(String[] args) {
-        
+        utilidades util= new utilidades();
+        int[]a=new int[5];
+        int[]b=new int[5];
+        System.out.println(""" 
+                           MENU PRINCIPAL
+                           1) A DIFERENCIA B
+                           2) A SIMETRIA B
+                           3) SALIR DEL PROGRAMA
+                           """);
     }
     
 }
