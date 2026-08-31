d9aab95b4ce1ad007a7c441295583a8a4d0bbe42
patrick <patrick>
2026-08-31 07:08:19 -0400

    Creacion de los metodos imprimir y diferencia AB en utilidades y algunos agregados en principal

--- src/examen_git/principal.java
+++ src/examen_git/principal.java
@@ -1,15 +1,35 @@
 package examen_git;
+
+import java.util.Scanner;
+
 public class principal {
     public static void main(String[] args) {
+        
+        Scanner l=new Scanner(System.in);
         utilidades util= new utilidades();
         int[]a=new int[5];
         int[]b=new int[5];
+        for (int i = 0; i <a.length; i++) {
+            System.out.println("Ingrese un valor para a");
+            a[i]=l.nextInt();
+            System.out.println("Ingrese un valor para b");
+            b[i]=l.nextInt();
+        }
         System.out.println(""" 
                            MENU PRINCIPAL
                            1) A DIFERENCIA B
                            2) A SIMETRIA B
                            3) SALIR DEL PROGRAMA
                            """);
+        int op=l.nextInt();
+        switch(op){
+            case 1:
+                util.diferenciaAB(a, b);
+            case 2:
+            case 3:
+            default:
+        }
     }
+    
     
 }
--- src/examen_git/utilidades.java
+++ src/examen_git/utilidades.java
@@ -1,4 +1,48 @@
 package examen_git;
 public class utilidades {
-    
+    public void imprimir(int[] a,int[] b){
+        System.out.println("los datos del conjunto A son:");
+        for (int i = 0; i <a.length; i++) {
+            System.out.println(a[i]);
+        }
+        System.out.println("Los datos del conjunto B son:");
+        for (int i = 0; i <b.length; i++) {
+            System.out.println(b[i]);
+        }
+    }
+    public void diferenciaAB(int []a, int[] b){
+        int cont=0;
+        int insercion=0;
+        int rep=0;
+        for (int i = 0; i < a.length; i++) {
+            rep=0;
+            for (int j = 0; j < b.length; j++) {
+                if (a[i]==b[i]) {
+                    rep=rep+1;
+                }
+            }
+            if (rep==0) {
+            cont=cont+1;
+            }
+        }
+        int[] dif=new int[cont];
+        for (int i = 0; i < a.length; i++) {
+            rep=0;
+            for (int j = 0; j < b.length; j++) {
+                if (a[i]==b[i]) {
+                    rep=rep+1;
+                }
+            }
+            if (rep==0) {
+                if (insercion<cont) {
+                dif[insercion]=a[i];
+                insercion=insercion+1;
+                }
+            }
+        }
+        System.out.println("los datos de la diferencia A B son");
+        for (int i = 0; i < dif.length; i++) {
+            System.out.println(dif[i]);
+        }
+    }
 }
